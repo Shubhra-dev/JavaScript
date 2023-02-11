4825d42baf7107545606981ff345282a03d1711a
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-02-11 16:24:31 +0600

    Rolling the dice and adding it to current Score

--- PIG game/script.js
+++ PIG game/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const name0Element = document.getElementById('name--0');
+const name1Element = document.getElementById('name--1');
+const diceElement = document.querySelector('.dice');
+
+const btNew = document.querySelector('.btn--new');
+const btRoll = document.querySelector('.btn--roll');
+const btHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+btRoll.addEventListener('click', function () {
+  const dicePoint = Math.trunc(Math.random() * 6) + 1;
+
+  diceElement.src = `dice-${dicePoint}.png`;
+  diceElement.classList.remove('hidden');
+  if (dicePoint !== 1) {
+    currentScore += dicePoint;
+    current0Element.textContent = currentScore;
+  } else {
+    currentScore = 0;
+  }
+});
