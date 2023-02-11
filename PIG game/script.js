3dc5d88103a6f7b3f54e8d80f0cd54d0f6df6e84
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-02-11 16:44:20 +0600

    Switching Players and Separate Current Scores

--- PIG game/script.js
+++ PIG game/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const player0Active = document.querySelector('.player--0');
+const player1Active = document.querySelector('.player--1');
+
 const score0Element = document.getElementById('score--0');
 const score1Element = document.getElementById('score--1');
 const current0Element = document.getElementById('current--0');
@@ -12,7 +15,19 @@ const btNew = document.querySelector('.btn--new');
 const btRoll = document.querySelector('.btn--roll');
 const btHold = document.querySelector('.btn--hold');
 
+const updateCurrentScore = function (player, score) {
+  player.textContent = score;
+};
+
+const addActiveClass = function (player) {
+  player.classList.add('player--active');
+};
+const removeActiveClass = function (player) {
+  player.classList.remove('player--active');
+};
+
 let currentScore = 0;
+let activePlayer = 0;
 
 btRoll.addEventListener('click', function () {
   const dicePoint = Math.trunc(Math.random() * 6) + 1;
@@ -21,8 +36,23 @@ btRoll.addEventListener('click', function () {
   diceElement.classList.remove('hidden');
   if (dicePoint !== 1) {
     currentScore += dicePoint;
-    current0Element.textContent = currentScore;
+    if (activePlayer === 0) {
+      updateCurrentScore(current0Element, currentScore);
+    } else {
+      updateCurrentScore(current1Element, currentScore);
+    }
   } else {
+    if (activePlayer === 0) {
+      updateCurrentScore(current0Element, 0);
+      removeActiveClass(player0Active);
+      activePlayer = 1;
+      addActiveClass(player1Active);
+    } else {
+      updateCurrentScore(current1Element, 0);
+      removeActiveClass(player1Active);
+      activePlayer = 0;
+      addActiveClass(player0Active);
+    }
     currentScore = 0;
   }
 });
