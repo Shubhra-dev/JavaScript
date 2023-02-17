c9f06e1ce344d6e2bc49ee14ee9e67e59b980eb2
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-02-17 16:56:26 +0600

    Add files via upload

---
+++ Higher order & First Class Functions/script.js
@@ -0,0 +1,22 @@
+'use strict';
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//higher order Functions -> functions taking as a parameter
+const transFormer = function (str, fun) {
+  console.log(`Original : ${str}`);
+
+  console.log(`Transformed: ${fun(str)}`);
+
+  console.log(`Transformed by: ${fun.name}`);
+};
+
+//first class function -> functions use as value
+transFormer('JavaScript is the best!!!!!', upperFirstWord);
+transFormer('JavaScript is the best!!!!!', oneWord);
