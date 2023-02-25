0bd3b03e8db560fd380bd537aa94f20c1f3916e0
Shubhradev Chakrabarty <shuuvro.dev@gmail.com>
2023-02-25 12:35:51 +0600

    Modal Window and Smooth Scrolling added

---
+++ Bankist Website/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScroll = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+btnScroll.addEventListener('click', function (e) {
+  //e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
