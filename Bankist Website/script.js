b33ccc9d4878ecaec5f230ce498a5de31b637fea
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-02-26 16:53:57 +0600

    Menu opacity on selecting menu bar feature added

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -68,3 +68,26 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${contentNum}`)
     .classList.add('operations__content--active');
 });
+
+const nav = document.querySelector('.nav');
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const parent = link.closest('.nav');
+    const siblings = parent.querySelectorAll('.nav__link');
+    const logo = parent.querySelector('img');
+
+    siblings.forEach(function (el) {
+      if (el !== link) {
+        el.style.opacity = logo.style.opacity = opacity;
+      }
+    });
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
