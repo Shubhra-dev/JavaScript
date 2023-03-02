d00a48c2bb5eea8a04a757deda61d4182b2ced03
Shubhradev Chakrabarty <shuuvro.dev@gmail.com>
2023-03-02 23:29:16 +0600

    Lazy load function added

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -188,6 +188,20 @@ document.addEventListener('keydown', function (e) {
 
 // Lazy load images
 
+const targetImages = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0.05,
