072bc9b405c96038448a78815f153f62539bc52e
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-03-09 15:52:53 +0600

    Update script.js

--- Higher order & First Class Functions/script.js
+++ Higher order & First Class Functions/script.js
@@ -1,3 +1,4 @@
+//strict mode
 'use strict';
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
@@ -33,7 +34,6 @@ const greetHi = greeting('Hi');
 greetHi('Adam');
 greeting('Hello')('Mr');
 
-// Arrow Function
 const greetArr = greeting => names => console.log(`${greeting} ${names}`);
 greetArr('Hello')('Mrs');
 
