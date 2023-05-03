eb27c28acfa0d5324e32a8f020f2b962ab0b9522
Shubhradev Chakrabarty <shuuvro.dev@gmail.com>
2023-05-03 11:26:08 +0600

    Map Marker on click Added

--- Mapty/script.js
+++ Mapty/script.js
@@ -11,3 +11,39 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coord = [latitude, longitude];
+      var map = L.map('map').setView(coord, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (me) {
+        const { lat, lng } = me.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+
+    function () {
+      alert('Can get location');
+    }
+  );
+}
