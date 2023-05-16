e5199b68bf881162d2b8a7bf23dad222d42e6cd1
Shubhradev Chakrabarty <shuuvro.dev@gmail.com>
2023-05-16 11:32:01 +0600

    GetImage function added for creating new image element

--- AJAX & API/Chaining Promises/script.js
+++ AJAX & API/Chaining Promises/script.js
@@ -7,3 +7,17 @@ const wait = function (sec) {
     setTimeout(resolve, sec * 1000);
   });
 };
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imageContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
